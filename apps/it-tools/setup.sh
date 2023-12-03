06f477798c7abf23d773f3a8aa5301fc0568eda6
Stan Hebben <stan@stanhebben.net>
2023-12-03 23:20:24 +0100

    Added it-tools and marked the send service as broken for the time being (until an arm64 build is available)

--- apps/it-tools/setup.sh
+++ apps/it-tools/setup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 read -p "Domain name: " APP_DOMAIN
-DB_PASSWORD=`openssl rand -base64 16`
 
-APP_DIR=/mnt/ssd/apps/send
-LOCAL_DATA_DIR=/mnt/ssd/apps-local/send
+APP_DIR=/mnt/ssd/apps/it-tools
 LOCAL_IPV4=`uci get network.lan.ipaddr`
 LOCAL_IPV6_PREFIX=`uci get network.globals.ula_prefix`
 LOCAL_IPV6=${LOCAL_IPV6_PREFIX/::\/48/::1}
 
 mkdir -p $APP_DIR
 echo "APP_DOMAIN=$APP_DOMAIN" >> $APP_DIR/.env
-echo "LOCAL_DATA_DIR=$LOCAL_DATA_DIR" >> $APP_DIR/.env
 
 cp docker-compose.yml $APP_DIR/
 
