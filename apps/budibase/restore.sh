c3cce4681b7b39eec67e2b019190ee1ac7d28fcc
Stan Hebben <stan@stanhebben.net>
2023-12-04 20:04:23 +0100

    Added budibase

---
+++ apps/budibase/restore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source .env
+
+docker compose up -d
+
+if ! grep -q "$APP_DOMAIN" /etc/hosts; then
+  LOCAL_IPV4=`uci get network.lan.ipaddr`
+  LOCAL_IPV6_PREFIX=`uci get network.globals.ula_prefix`
+  LOCAL_IPV6=${LOCAL_IPV6_PREFIX/::\/48/::1}
+  echo "$LOCAL_IPV4 $APP_DOMAIN" >> /etc/hosts
+  echo "$LOCAL_IPV6 $APP_DOMAIN" >> /etc/hosts
+  service dnsmasq restart
+fi
